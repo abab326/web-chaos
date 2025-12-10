511ed45cf85d9a2021324b74c511bf2c1d3cc322
liushuxue <abab326@163.com>
2025-12-10 23:28:58 +0800

    tailwindcss 加载本地svg 图片

--- src/views/router-child/child.vue
+++ src/views/router-child/child.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1>Router Child Child</h1>
+    <h1 class="text-red-800">Router Child Child</h1>
+    <i class="i-[icon--message] text-red-500 text-xl"></i>
   </div>
 </template>
 
